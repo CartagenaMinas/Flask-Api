c5d6734d99f4962b7914b829b30081284a583c78
CartagenaMinas <79287172+CartagenaMinas@users.noreply.github.com>
2021-06-04 21:42:58 -0500

    primer commit a Heroku

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import  Flask #importamos
+from flask_cors import CORS
+
+
+app=Flask(__name__) #Instanciamos nuestra aplicacion
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return "Hello, World"
+
+
+@app.route('/predict')
+def predict():
+    return "predicciones"
+
+if __name__ == "__main__":
+    app.run()
